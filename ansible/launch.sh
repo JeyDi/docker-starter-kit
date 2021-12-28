8453cab56134015a4cee6a352f5523280748ed46
Andrea Guzzo <>
2021-12-28 17:18:46 +0100

    new version

--- ansible/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "launching provision-vps.yml"
-# ansible-playbook -i inventory.ini -l "personal_setup" provision-vps.yml
-ansible-playbook -i inventory.ini provision-vps.yml --tags "docker"
-echo "finish..."
-
-
