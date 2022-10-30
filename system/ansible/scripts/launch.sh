ca24035e36e4b3d87f594739596a597b27de463c
Andrea Guzzo <andrea.guzzo92@gmail.com>
2022-10-30 18:54:45 +0100

    added updates for bluepipe

--- system/ansible/scripts/launch.sh
+++ system/ansible/scripts/launch.sh
@@ -2,7 +2,7 @@
 # use --ask-pass if you want to ask password auth
 echo "launching starting configuration: start_config.yml"
 ansible-galaxy collection install community.general
-ansible-playbook -i inventory.ini -l "pbg"  playbooks/start_config.yml --ask-pass
+ansible-playbook -i inventory.ini -l "bluepipe"  playbooks/start_config.yml --ask-pass
 # ansible-playbook -i inventory.ini -l "gpu2" playbooks/start_config_internal.yml --tags "gitlab" --ask-pass
 # ansible-playbook -i inventory.ini playbooks/start_config_internal.yml --tags "users" --ask-pass
 echo "finish..."
